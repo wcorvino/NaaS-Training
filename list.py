1cc5422209b5b7f8c53328f90b22158dd75115e0
wcorvino <wcorvino@verizon.net>
2021-08-29 11:00:36 -0400

    update

---
+++ list.py
@@ -0,0 +1,5 @@
+my_list = ["January", "February", "March"]
+print(my_list[2])
+my_list.append("April")
+print(my_list)
+print(my_list[3])
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# Globals
 k_sec_day = 24*60*60
 unit = "Days"
 unit_conv = "Seconds"
 
-
+# Functions
 def days_to_unitc(number_of_days):
     return f"{number_of_days} {unit} equals {number_of_days * k_sec_day} {unit_conv}"
 
@@ -10,6 +11,7 @@ def days_to_unitc(number_of_days):
 def validate_and_execute():
     try:
         user_input_number = int(numb_of_days_element)
+        # conversion only for pos integers
         if user_input_number > 0:
             calculated_value = days_to_unitc(user_input_number)
             print(calculated_value)
@@ -21,6 +23,7 @@ def validate_and_execute():
         print("err.enter integer ot comma delimited list of values")
 
 
+# Run
 user_input = ""
 while user_input != "exit":
     user_input = input("Enter value  [Days]\n")
