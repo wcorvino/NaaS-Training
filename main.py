d01e7dc67bdfa361b669c7852b426dacc9fe3154
wcor <wcorvino@verizon.net>
2021-08-29 08:09:58 -0400

    update

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+k_sec_day = {24*60*60}
+unit = "Days"
+unitc = "Seconds"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def days_to_unitc(number_of_days):
+    return f"{number_of_days}{unit}equals{number_of_days*k_sec_days}{unitc}"
+
+def validate_and_execute():
+    try:
+        user_input_number=int(num_of_days_element)
+        if user_input_number>0:
+            calculated_value=days_to_unitc(user_input_number)
+            print(calculated_value)
+        elif use_input_number == 0:
+            print("err.entered 0")
+        else:
+            print("err.entered neg")
+    except ValueError:
+        print("err.enter integer ot comma delimited list of values")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+user_input=""
+while user_input!= "exit":
+    user_input = input("Enter value  [Days]\n")
+    print(type(user_input.split(",")))
+    print(user_input.splt(","))
+    for numb_of_days_element in user_input.splt(","):
+        validate_and_execute()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
