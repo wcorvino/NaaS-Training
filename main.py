a32e236630f79b523e86debc161e40d4cc039298
wcor <wcorvino@verizon.net>
2021-08-29 08:42:44 -0400

    fixed

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 k_sec_day = {24*60*60}
 unit = "Days"
-unitc = "Seconds"
+unit_conv = "Seconds"
+
 
 def days_to_unitc(number_of_days):
-    return f"{number_of_days}{unit}equals{number_of_days*k_sec_days}{unitc}"
+    return f"{number_of_days}{unit}equals{number_of_days*k_sec_day}{unit_conv}"
+
 
 def validate_and_execute():
     try:
-        user_input_number=int(num_of_days_element)
-        if user_input_number>0:
-            calculated_value=days_to_unitc(user_input_number)
+        user_input_number = int(numb_of_days_element)
+        if user_input_number > 0:
+            calculated_value = days_to_unitc(user_input_number)
             print(calculated_value)
-        elif use_input_number == 0:
+        elif user_input_number == 0:
             print("err.entered 0")
         else:
             print("err.entered neg")
@@ -19,11 +21,10 @@ def validate_and_execute():
         print("err.enter integer ot comma delimited list of values")
 
 
-user_input=""
-while user_input!= "exit":
+user_input = ""
+while user_input != "exit":
     user_input = input("Enter value  [Days]\n")
     print(type(user_input.split(",")))
-    print(user_input.splt(","))
-    for numb_of_days_element in user_input.splt(","):
+    print(user_input.split(","))
+    for numb_of_days_element in user_input.split(","):
         validate_and_execute()
-
