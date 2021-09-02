460e77005632bb52fc56d87ebbd5c08335182978
wcorvino <wcorvino@verizon.net>
2021-09-02 13:15:31 -0400

    New

--- Project2-Python-Google/basic/main1.py
+++ Project2-Python-Google/basic/main1.py
@@ -1,17 +1,28 @@
 
-def swap(x, y, n):
-    z = x[n]
-    x = y[n]
-    y = z[0]
+list1 = ['aa', 'xx', 'zz']
+list2 = ['bb', 'cc']
+
+
+def process(list_a, list_b):
+    result = []
+    while len(list_a) and len(list_b):
+
+        if list_a[0] < list_b[0]:
+            result.append(list_a.pop(0))
+        else:
+            result.append(list_b.pop(0))
+
+    result.extend(list_a)
+    result.extend(list_b)
+    return result
 
 
 def main():
-    list1 = ['aa', 'xx', 'zz']
-    list2 = ['bb', 'cc']
-    list_a = list1+list2
-    list_n = []
+    print process(list1,list2)
 
-    swap(list_n, list_a, 0)
+
+
+
 
 
 if __name__ == '__main__':
